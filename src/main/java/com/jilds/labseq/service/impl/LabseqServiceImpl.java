1e721e029516563c6ecf73e6279b64caa3958065
jilds <jilds@outlook.com>
2023-05-20 18:30:58 +0100

    final implementation with caching

--- src/main/java/com/jilds/labseq/service/impl/LabseqServiceImpl.java
+++ src/main/java/com/jilds/labseq/service/impl/LabseqServiceImpl.java
@@ -2,17 +2,23 @@ package com.jilds.labseq.service.impl;
 
 import com.jilds.labseq.exception.LabseqException;
 import com.jilds.labseq.service.LabseqService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class LabseqServiceImpl implements LabseqService {
 
+    private final Map<Integer, BigInteger> localCache = new HashMap<>();
+
     @Override
     public BigInteger doTheMath(Integer value) {
+
         if (value < 0) {
-            throw new LabseqException("The index value must be a non-negative integer");
+            throw new LabseqException("The value must be equal or greater then 0", HttpStatus.BAD_REQUEST);
         }
 
         if (value == 0 || value == 2) {
@@ -23,7 +29,13 @@ public class LabseqServiceImpl implements LabseqService {
             return BigInteger.ONE;
         }
 
+        if (localCache.containsKey(value)){
+            return localCache.get(value);
+        }
+
         BigInteger result = doTheMath(value - 4).add(doTheMath(value - 3));
+
+        localCache.put(value, result);
 
         return result;
     }

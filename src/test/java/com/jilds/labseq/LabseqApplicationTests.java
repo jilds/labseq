89aca7878cd2919a5725887f6d56856b3295efe8
jilds <jilds@outlook.com>
2023-05-20 16:29:32 +0100

    initial commit

---
+++ src/test/java/com/jilds/labseq/LabseqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jilds.labseq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabseqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
